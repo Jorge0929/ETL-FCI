adcccd9c75a8979691af63f89dbb5dc2c2b88521
Jorge0929 <jorgeandresabino123@gmail.com>
2026-06-17 15:32:56 -0500

    feat(config): add remaining Colsubsidio modules

---
+++ config.py
@@ -0,0 +1,125 @@
+MODULES_COLSUBSIDIO = {
+    "Registro_empresas": [
+        "Name", "Raz_n_social_de_la_empresa", "Nombre_del_contacto_principal",
+        "Correo_electr_nico_del_contacto", "Tel_fono",
+        "Sector_econ_mico_de_la_empresa", "Tama_o_de_la_empresa",
+        "Municipio", "Departamento", "Direcci_n_de_la_empresa",
+        "Cluster", "Cargo", "Fecha_de_registro", "Estado_en_la_ruta",
+        "Estado_diagn_stico", "Fecha_diagn_stico_programado",
+        "Fecha_diagn_stico_ejecutado", "Estado_asesor_a",
+        "Fecha_asesor_a_programada", "Fecha_asesor_a_ejecutada",
+        "Estado_m_dulo_1", "Fecha_m_dulo_1_programado",
+        "Fecha_m_dulo_1_ejecutado", "Estado_m_dulo_2",
+        "Fecha_m_dulo_2_programado", "Fecha_m_dulo_2_ejecutado",
+        "Estado_sensibilizaci_n", "Fecha_sensibilizaci_n_ejecutada",
+        "Estado_transferencia", "Fecha_transferencia_programada",
+        "Fecha_transferencia_ejecutada", "Owner", "Created_By", "Modified_By",
+    ],
+    "Agenda_acompa_amiento": [
+        "Name", "Empresa", "Raz_n_social_de_la_empresa", "Tipo_de_servicio",
+        "Modalidad", "Cluster", "Fecha_y_horario", "Fecha_y_hora_inicio",
+        "Fecha_en_que_realiza_la_solicitd", "Profesional_asignado",
+        "Estado_de_asignaci_n", "Estado_de_la_ruta",
+        "Nombre_del_asesor_a_que_solicita_el_servicio",
+        "Observaci_n_de_agenda", "Direcci_n_sede", "Cantidad",
+        "Owner", "Created_By", "Modified_By",
+    ],
+    "Agenda_inscripci_n": [
+        "field", "Owner", "Cluster", "Connected_To__s", "Email", "Correo_electr_nico_2", 
+        "Secondary_Email", "Created_By", "Empresa", "Estado_de_la_ruta", "Tag", 
+        "Fecha_en_que_se_realiza_la_solicitud", "Modified_By", "Name", "Email_Opt_Out", 
+        "Nombre_Completo", "Nombre_Completo_2", "Nombre_del_asesoro_a_que_solicita_el_servicio", 
+        "N_mero_de_documento", "N_mero_de_documento_2", "Record_Image", "Raz_n_social_de_la_empresa",
+        "Rol", "Rol_2", "Seleccione_el_m_dulo", "Seleccione_fecha"
+    ],
+    "Diagn_stico":[
+        "Profesional_asignado","Agenda_origen","Connected_To__s","Email","Secondary_Email",
+        "Created_By","Owner","Empresa","Estado_del_diagnostico","Tag","Fecha_y_hora_fin",
+        "Fecha_y_hora_inicio","Informe_PDF","Modified_By","Email_Opt_Out","Name",
+        "Conclusiones","Profesional_asignado1","Record_Image","Recomendaciones"
+    ],
+    "Asesor_a":[
+        "Profesional_asignado", "Agenda","Owner", "Connected_To__s", "Email",
+        "Secondary_Email", "Created_By","Empresa", "Estado_de_la_asesor_a",
+        "Tag", "Fecha_y_hora_fin", "Fecha_y_hora_inicio", "Informe_PDF",
+        "Modified_By", "Email_Opt_Out", "Name", "Conclusiones", "Profesional_asignado1",
+        "Record_Image", "Recomendaciones"
+    ],
+    "Asesor_a_vacantes":[
+        "Profesional_asignado","Agenda","Owner", "Connected_To__s", "Email",
+        "Secondary_Email", "Created_By","Empresa", "Estado_de_la_asesor_a", "Tag",
+        "Fecha_y_hora_fin", "Fecha_y_hora_inicio", "Informe_PDF", "Modified_By",
+        "Email_Opt_Out", "Name", "Observaciones", "Profesional_asignado1", "Record_Image",
+        "Recomendaciones"
+    ],
+    "Sensibilizaci_n":[
+        "Profesional_asignado","Agenda", "Connected_To__s", "Email",
+        "Secondary_Email", "Created_By","Empresa", "Estado_de_la_sensibilizaci_n", "Tag", 
+        "Fecha_y_hora_fin", "Fecha_y_hora_inicio", "Informe_PDF", "Modified_By", 
+        "Email_Opt_Out", "Name", "Conclusiones", "Profesional_asignado1", "Record_Image",
+        "Recomendaciones", "Owner"
+    ],
+    "Transferencia":[
+        "Profesional_asignado","Agenda", "Connected_To__s", "Email", "Secondary_Email",
+        "Created_By","Empresa", "Estado_de_la_transferencia", "Tag", "Fecha_y_hora_fin", 
+        "Fecha_y_hora_inicio", "Informe_PDF", "Modified_By", "Email_Opt_Out", "Name", 
+        "Conclusiones", "Profesional_asignado1", "Record_Image", "Recomendaciones", "Owner"
+    ],
+    "Profesional":[
+        "Jornada_fin", "Jornada_de_inicio", "Cargo_Rol", "Connected_To__s", "Email", 
+        "Secondary_Email", "Created_By", "D_as_disponibles", "Estado", "Tag", 
+        "Jornada_de_inicio1", "Jornada_fin1", "M_ximo_citas_por_d_a", "Modalidad", 
+        "Modified_By", "Email_Opt_Out", "Name", "Record_Image", "Owner"
+    ],
+    "Participantes_Bootcamps":[
+        "Agendamiento_grupal_origen","Connected_To__s", "Email", "Secondary_Email", "Created_By","Empresa", 
+        "Estado_del_bootcamp", "Tag", "Modified_By", "Name", "Email_Opt_Out","Nombre_Completo", "N_mero_de_documento",
+        "Record_Image", "Owner", "Rol",
+    ],
+    "Modulo_1": [
+        "Asistencia_participante_2", "Asistencia_participante_1",
+        "Agendamiento_grupal_origen", "Asistencia_participante_11",
+        "Asistencia_participante_21", "Cluster", "Connected_To__s", "Email",
+        "Correo_electr_nico_1", "Correo_electr_nico_2", "Secondary_Email",
+        "Created_By", "Empresa", "Estado_del_m_dulo", "Tag", "Evidencia",
+        "Fecha_m_dulo", "Modified_By", "Email_Opt_Out", "Nombre_Completo", 
+        "Nombre_Completo_2","Name", "N_mero_documento", "N_mero_documento_2",
+        "Observaciones_del_participante_1", "Observaciones_del_participante_2",
+        "Observaciones_generales", "Record_Image", "Owner",
+        "Raz_n_social_empresa", "Rol", "Rol_2",
+    ],
+    "Modulo_2": [
+        "Asistencia_participante_2", "Asistencia_participante_1",
+        "Agendamiento_grupal_origen", "Asistencia_participante_11",
+        "Asistencia_participante_21", "Cluster", "Connected_To__s", "Email",
+        "Correo_electr_nico_1", "Correo_electr_nico_2", "Secondary_Email",
+        "Created_By", "Empresa", "Estado_del_m_dulo", "Tag", "Evidencia",
+        "Fecha_m_dulo", "Modified_By", "Email_Opt_Out", "Nombre_Completo",
+        "Nombre_Completo_2", "Name", "N_mero_documento", "N_mero_documento_2",
+        "Observaciones_del_participante_1", "Observaciones_del_participante_2",
+        "Observaciones_generales", "Record_Image", "Owner",
+        "Raz_n_social_empresa", "Rol", "Rol_2",
+    ],
+    "Intermediaci_n_RE": [
+        "Asistencia", "Asistencia_2", "Asistencia_3", "Asistencia_4",
+        "Asistencia_5", "Asistencia_6", "Asistencia_7", "Asistencia_8",
+        "Cargo", "Connected_To__s", "Email", "Secondary_Email", "Created_By",
+        "Tag", "Fecha_sesi_n", "Fecha_sesi_n_2", "Fecha_sesi_n_3",
+        "Fecha_sesi_n_4", "Fecha_sesi_n_5", "Fecha_sesi_n_6", "Fecha_sesi_n_7",
+        "Fecha_sesi_n_8", "Modified_By", "Email_Opt_Out", "Nombre_Completo",
+        "Name", "Observaciones", "Record_Image", "Owner", "Sede",
+    ],
+    "Asistencia_Formaci_n_LS": [
+        "Asistencia", "Asistencia_2", "Asistencia_3", "Asistencia_4",
+        "Asistencia_5", "Asistencia_6", "Asistencia_7", "Asistencia_8",
+        "Cargo", "Connected_To__s", "Email", "Secondary_Email", "Created_By",
+        "Tag", "Fecha_sesi_n", "Fecha_sesi_n_2", "Fecha_sesi_n_3",
+        "Fecha_sesi_n_4", "Fecha_sesi_n_5", "Fecha_sesi_n_6", "Fecha_sesi_n_7",
+        "Fecha_sesi_n_8", "Modified_By", "Email_Opt_Out", "Nombre_Completo",
+        "Name", "Observaciones", "Record_Image", "Owner", "Sede",
+    ]
+    
+}
+MODULES_CUIDARTE = {
+
+}
